62769411dbde7a8bd36372fa9db5a2ceb35c0eaa
HARSH MISHRA <71396592+hm3478@users.noreply.github.com>
2022-04-21 00:58:42 +0530

    Add files via upload

---
+++ food_order/admin/index.php
@@ -0,0 +1,51 @@
+<html>
+    <head>
+        <title>Food Order Website - Home Page</title>
+        <link rel="stylesheet" href="../css/admin.css">
+    </head>
+    <body>
+        <!-- Menu Section Starts -->
+        <div class="menu text-center">
+            <div class="wrapper">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Admin</a></li>
+                    <li><a href="#">Category</a></li>
+                    <li><a href="#">Food</a></li>
+                    <li><a href="#">Order</a></li>
+                </ul>
+            </div>
+        </div>
+        <!-- Menu Section Ends -->
+
+        <!-- Main Content Section Starts -->
+        <div class="main-content">
+            <div class="wrapper">
+                <h1>Dashboard</h1>
+                <div class="col-4 text-center">
+                    <h1>5</h1> Categories
+                </div>
+                <div class="col-4 text-center">
+                    <h1>5</h1> Categories
+                </div>
+                <div class="col-4 text-center">
+                    <h1>5</h1> Categories
+                </div>
+                <div class="col-4 text-center">
+                    <h1>5</h1> Categories
+                </div>
+
+                <div class="clearfix"></div>
+            </div>
+        </div>
+        <!-- Main Content Section Ends -->
+
+        <!-- Footer Section Starts -->
+        <div class="footer text-center">
+            <div class="wrapper">
+            <p>2022 All rights reserved, Some Restauran. Developed By - Isabel Cheung</p>
+            </div>
+        </div>
+        <!-- Footer Section Ends -->
+    </body>
+</html>
